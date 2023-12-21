64dbfc5c9f50d63cb0561fb448db9e01c483658c
aL0NEW0LF <mohamedfakhreeddine2019@gmail.com>
2023-12-21 18:28:36 +0100

    feat: feat selection demo

--- logic/data_preprocessing.py
+++ logic/data_preprocessing.py
@@ -1,7 +1,19 @@
 import pandas as pd
 import numpy as np
 from sklearn.feature_selection import VarianceThreshold, SelectKBest, f_classif
+import tkinter as tk
 
-class feature_selection():
-    class feature_selection_kBestFeatures():
-        pass
+
+def feature_selection_kBestFeatures(df: list | pd.DataFrame, k: int):
+    X = df[:, :-1]
+    y = df[:, -1]
+    skb = SelectKBest(score_func=f_classif, k=k)
+
+    return pd.concat([pd.DataFrame(skb.fit_transform(X, y)), pd.DataFrame(y)], axis=1)
+
+def feature_selection_varianceThreshold(df: list | pd.DataFrame, threshold: float):
+    X = df[:, :-1]
+    y = df[:, -1]
+    vt = VarianceThreshold(threshold=threshold)
+
+    return pd.DataFrame(vt.fit_transform(X, y))
--- main.py
+++ main.py
@@ -7,9 +7,11 @@ import pandas as pd
 from logic.file_handling import file_handling as fh
 from pandastable import Table, TableModel
 from tksheet import Sheet
+from logic.data_preprocessing import feature_selection_kBestFeatures, feature_selection_varianceThreshold
 
 LARGEFONT = ("montserrat", 24)
 
+# WRAPPER FUNCTIONS
 def UploadAction():
     file_path = ctk.filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx"), ("CSV files", "*.csv"), ("JSON files", "*.json"), ("Text files", "*.txt")])
     print('Selected:', file_path)
@@ -22,24 +24,28 @@ def UploadAction():
             global DATA
             DATA = fh(file_path=file_path, file_extension=file_extension)
             print(DATA)
-        return None
+        return
 
     except ValueError:
         ctk.messagebox.showerror("Information", "The file you have chosen is invalid")
-        return None
+        return
     except FileNotFoundError:
         ctk.messagebox.showerror("Information", f"No such file as {file_path}")
-        return None
+        return
 
 def read_data():
     global DATA
     DATA.file_data_read()
     print(DATA)
 
+def kbestFeat_Selec_event():
+    dialog = ctk.CTkInputDialog(text="Type in a number:", title="Test")
+    print("Number:", dialog.get_input())
+
+
+# MAIN APP
 class App(ctk.CTk):
-    # __init__ function for class cApp
     def __init__(self, *args, **kwargs):
-        # __init__ function for class CTk
         ctk.CTk.__init__(self, *args, **kwargs)
 
         self.geometry("1380x720")
@@ -48,7 +54,6 @@ class App(ctk.CTk):
         self.minsize(1380, 720)
         self.configure(fg_color="#161616")
 
-        # creating a container
         container = ctk.CTkFrame(self, width=self.winfo_width(), height=self.winfo_height())
         container.configure(fg_color="#101010")
         container.pack(side="bottom", expand=True, fill="both", padx=24, pady=24)
@@ -56,25 +61,18 @@ class App(ctk.CTk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
-        # initializing frames to an empty array
         self.frames = {}
 
-        # iterating through a tuple consisting
-        # of the different page layouts
-        for F in (StartPage, RegressionPage, DecisionTreePage, NaiveBayesPage, SVMPage, KmeansPage, KNNPage):
+
+        for F in (StartPage, RegressionPage, DecisionTreePage, NaiveBayesPage, SVMPage, KmeansPage, KNNPage, VarianceThresholdPage, KbestfeatPage):
             frame = F(container, self)
 
-            # initializing frame of that object from
-            # startpage, page1, page2 respectively with
-            # for loop
             self.frames[F] = frame
 
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
 
-    # to display the current frame passed as
-    # parameter
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.configure(fg_color="#101010")
@@ -167,7 +165,6 @@ class StartPage(ctk.CTkFrame):
         KNNButton.grid(row=1, column=2, padx=20, pady=20, sticky="nw")
 
 
-
 class RegressionPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
         ctk.CTkFrame.__init__(self, parent)
@@ -181,33 +178,58 @@ class RegressionPage(ctk.CTkFrame):
         frame1 = ctk.CTkFrame(self, fg_color="#101010")
         frame1.grid(row=1, column=0, columnspan=5, ipadx=8, ipady=8, sticky="ew")
 
+        button2 = ctk.CTkButton(frame1, image=backImg, text="", command=lambda: controller.show_frame(StartPage))
+        button2.grid(row=0, column=0, padx=4, pady=8, ipadx=8, ipady=8, sticky="w")
+
         button = ctk.CTkButton(frame1, text="Upload your data file", command= UploadAction)
-        button.grid(row=0, column=0, padx=(0, 4), pady=8, ipadx=8, ipady=8, sticky="w")
+        button.grid(row=0, column=1, padx=(0, 4), pady=8, ipadx=8, ipady=8, sticky="w")
         
         button3 = ctk.CTkButton(frame1, text="Read data file", command= read_data)
-        button3.grid(row=0, column=1, padx=4, pady=8, ipadx=8, ipady=8, sticky="w")
+        button3.grid(row=0, column=2, padx=4, pady=8, ipadx=8, ipady=8, sticky="w")
 
         button1 = ctk.CTkButton(frame1, text="Print", command=lambda: print(DATA))
-        button1.grid(row=0, column=2, padx=4, pady=8, ipadx=8, ipady=8, sticky="w")
+        button1.grid(row=0, column=3, padx=4, pady=8, ipadx=8, ipady=8, sticky="w")
 
-        button4 = ctk.CTkButton(frame1, text="Load table", command=lambda: self.load_data(frame2))
-        button4.grid(row=0, column=3, padx=4, pady=8, ipadx=8, ipady=8, sticky="w")
+        button4 = ctk.CTkButton(frame1, text="Load table", command=lambda: self.load_data())
+        button4.grid(row=0, column=4, padx=4, pady=8, ipadx=8, ipady=8, sticky="w")
 
-        button2 = ctk.CTkButton(frame1, image=backImg, text="", command=lambda: controller.show_frame(StartPage))
-        button2.grid(row=0, column=4, padx=4, pady=8, ipadx=8, ipady=8, sticky="w")
+        optionmenu_var = ctk.StringVar(value="")
+        combobox = ctk.CTkOptionMenu(master=frame1,
+                                       values=["Variance threshold", "K-best features"],
+                                       command=lambda x: self.optionmenu_callback(x, controller),
+                                       variable=optionmenu_var,
+                                       width=150)
+        combobox.grid(row=0, column=5, padx=4, pady=8, ipadx=8, ipady=8, sticky="w")
 
         frame2 = ctk.CTkFrame(self, fg_color="#101010")
         #frame2.configure(fg_color="#101010")
         frame2.grid(row=2, column=0, columnspan=5, ipadx=8, ipady=8, sticky="nsew")
+        self.sheet = Sheet(frame2, data = None)
+        self.sheet.enable_bindings()
+        self.sheet.pack(side="top" , fill="both", expand=True)
 
-
-    def load_data(self, frame2):
+    def load_data(self):
         global DATA
-        sheet = Sheet(frame2, data = DATA.file_data.values.tolist(), auto_resize_columns = 100)
-        sheet.enable_bindings()
-        sheet.pack(side="top" , fill="both", expand=True)
+        self.sheet.set_sheet_data(data = DATA.file_data.values.tolist())
 
-    
+        """ if self.sheet is None:
+            self.sheet = Sheet(self.frame2, data = DATA.file_data.values.tolist())
+            self.sheet.enable_bindings()
+            self.sheet.pack(side="top" , fill="both", expand=True)
+        else:
+            self.sheet.set_sheet_data(data = DATA.file_data.values.tolist()) """
+ 
+    def optionmenu_callback(self, choice, controller):
+        if 'DATA' not in globals() or DATA.file_data is None:
+            tk.messagebox.showerror("Information", "Please upload a data file first")
+            return
+        
+        if choice == "Variance threshold":
+            controller.show_frame(VarianceThresholdPage)
+        elif choice == "K-best features":
+            # kbestFeat_Selec_event()
+            controller.show_frame(KbestfeatPage)
+
 class DecisionTreePage(ctk.CTkFrame):
     def __init__(self, parent, controller):
         ctk.CTkFrame.__init__(self, parent)
@@ -216,12 +238,7 @@ class DecisionTreePage(ctk.CTkFrame):
         label = ctk.CTkLabel(self, text="DecisionTreePage", text_color="#FFFFFF", font=LARGEFONT)
         label.grid(row=0, column=0, padx=10, pady=10)
 
-        # button to show frame 2 with text
-        # layout2
         button1 = ctk.CTkButton(self, image=backImg, text="", width=32, height=32, command=lambda: controller.show_frame(StartPage))
-
-        # putting the button in its place
-        # by using grid
         button1.grid(row=1, column=0, padx=8, pady=8)
 
 
@@ -233,12 +250,7 @@ class NaiveBayesPage(ctk.CTkFrame):
         label = ctk.CTkLabel(self, text="NaiveBayesPage", text_color="#FFFFFF", font=LARGEFONT)
         label.grid(row=0, column=0, padx=10, pady=10)
 
-        # button to show frame 2 with text
-        # layout2
         button1 = ctk.CTkButton(self, image=backImg, text="", width=32, height=32, command=lambda: controller.show_frame(StartPage))
-
-        # putting the button in its place
-        # by using grid
         button1.grid(row=1, column=0, padx=8, pady=8)
 
 
@@ -250,12 +262,7 @@ class SVMPage(ctk.CTkFrame):
         label = ctk.CTkLabel(self, text="SVMPage", text_color="#FFFFFF", font=LARGEFONT)
         label.grid(row=0, column=0, padx=10, pady=10)
 
-        # button to show frame 2 with text
-        # layout2
         button1 = ctk.CTkButton(self, image=backImg, text="", width=32, height=32, command=lambda: controller.show_frame(StartPage))
-
-        # putting the button in its place
-        # by using grid
         button1.grid(row=1, column=0, padx=8, pady=8)
 
 
@@ -267,12 +274,7 @@ class KmeansPage(ctk.CTkFrame):
         label = ctk.CTkLabel(self, text="KmeansPage", text_color="#FFFFFF", font=LARGEFONT)
         label.grid(row=0, column=0, padx=10, pady=10)
 
-        # button to show frame 2 with text
-        # layout2
         button1 = ctk.CTkButton(self, image=backImg, text="", width=32, height=32, command=lambda: controller.show_frame(StartPage))
-
-        # putting the button in its place
-        # by using grid
         button1.grid(row=1, column=0, padx=8, pady=8)
 
 
@@ -284,13 +286,62 @@ class KNNPage(ctk.CTkFrame):
         label = ctk.CTkLabel(self, text="KNNPage", text_color="#FFFFFF", font=LARGEFONT)
         label.grid(row=0, column=0, padx=8, pady=8)
 
-        # button to show frame 2 with text
-        # layout2
         button1 = ctk.CTkButton(self, image=backImg, text="", width=32, height=32, command=lambda: controller.show_frame(StartPage))
-
-        # putting the button in its place
-        # by using grid
         button1.grid(row=1, column=0, padx=8, pady=8)
+
+class VarianceThresholdPage(ctk.CTkFrame):
+    def __init__(self, parent, controller):
+        ctk.CTkFrame.__init__(self, parent)
+        backImg = ImageTk.PhotoImage(Image.open("./assets/icons/back.png").resize((32, 32), Image.LANCZOS))
+
+        label = ctk.CTkLabel(self, text="VarianceThresholdPage", text_color="#FFFFFF", font=LARGEFONT)
+        label.grid(row=0, column=0, padx=10, pady=10)
+
+        button1 = ctk.CTkButton(self, image=backImg, text="", width=32, height=32, command=lambda: controller.show_frame(RegressionPage))
+        button1.grid(row=1, column=0, padx=8, pady=8)
+
+class KbestfeatPage(ctk.CTkFrame):
+    def __init__(self, parent, controller):
+        ctk.CTkFrame.__init__(self, parent)
+        backImg = ImageTk.PhotoImage(Image.open("./assets/icons/back.png").resize((32, 32), Image.LANCZOS))
+
+        label = ctk.CTkLabel(self, text="KbestfeatPage", text_color="#FFFFFF", font=LARGEFONT)
+        label.grid(row=0, column=0, padx=10, pady=10)
+        
+        button1 = ctk.CTkButton(self, image=backImg, text="", width=32, height=32, command=lambda: controller.show_frame(RegressionPage))
+        button1.grid(row=1, column=0, padx=8, pady=8)
+        
+        K_entry = ctk.CTkEntry(self, width=10)
+        K_entry.grid(row=2, column=0, padx=8, pady=8)
+
+        button2 = ctk.CTkButton(self, text="Select features", command=lambda: self.kbestFeat_Selec_event(K_entry.get()))
+        button2.grid(row=3, column=0, padx=8, pady=8)
+
+    def kbestFeat_Selec_event(self, k):  
+        global DATA
+
+        if k == "":
+            tk.messagebox.showerror("Information", "Please enter a value for k")
+            return
+        
+        try:
+            k = int(k)
+        except ValueError:
+            tk.messagebox.showerror("Information", "Please enter a valid value for k")
+            return
+        
+        if k <= 0:
+            tk.messagebox.showerror("Value", "The value you chose for k is invalid")
+            return
+        elif k >= DATA.file_data.shape[1]:
+            tk.messagebox.showerror("Value", "The value you chose for k cant be greater than the number of features")
+            return
+        
+        DATA.file_data = feature_selection_kBestFeatures(DATA.file_data.values, k)
+        
+        global app
+
+        app.frames[RegressionPage].load_data()
 
 
 # Driver Code
